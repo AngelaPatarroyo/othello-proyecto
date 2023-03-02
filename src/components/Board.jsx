004ac805eafd52232f5895c58b3e23c01a3d0f2a
Angela Patarroyo <angelapatarroyo@Angelas-MacBook-Pro.local>
2023-03-02 14:51:56 -0500

    Primera Regla Resuelta

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 /* false = turno blanco
     true = turno negro */
@@ -8,53 +8,68 @@ import React, { useState } from 'react';
 0=vacio */
 
 const Board = () => {
-    const [turn, setTurn] = useState(true);
-    const [matriz, setMatriz] = useState(Array(8).fill(0).map(() => Array(8).fill(0)));
-    matriz[3][3] = 2;
-    matriz[4][3] = 1;
-    matriz[4][4] = 2;
-    matriz[3][4] = 1;
+  const [turn, setTurn] = useState(true);
+  const [matriz, setMatriz] = useState(
+    Array(8)
+      .fill(0)
+      .map(() => Array(8).fill(0))
+  );
+  matriz[3][3] = 2;
+  matriz[4][3] = 1;
+  matriz[4][4] = 2;
+  matriz[3][4] = 1;
 
-    const checkTile = (x, y) => {
-        if (matriz[x][y] !== 0) {
-            return false;
-        }
-        const value = turn ? 2 : 1;
-        setMatriz(matriz => {
-            const newMatriz = [...matriz];
-            newMatriz[x][y] = value;
-            return newMatriz;
-        });
-        setTurn(prevTurn => !prevTurn);
-    };
-    const Tile = ({ row, col, value, onClick }) => (
-        
-        <div onClick={() => onClick(row, col)} className="flex items-center justify-center bg-green-600 h-20 w-20 border-solid border-2 border-green-800">
-            {value !== 0 && (
-                <div className={`${value === 1 ? 'bg-white' : 'bg-black'} h-16 w-16 rounded-full`} />
-            )}
-        </div>
-    );
+  const checkTile = (x, y) => {
+    if (matriz[x][y] !== 0) {
+      return false;
+    }
+    const value = turn ? 2 : 1;
+    const oppositeColor = turn ? 1 : 2;
 
- 
-    return (
-        <div className="mt-14">
-            {matriz.map((row, rowIndex) => (
-                <div key={rowIndex} className="flex justify-start ml-44">
-                    {row.map((value, colIndex) => (
-                        <Tile
-                            key={colIndex}
-                            row={rowIndex}
-                            col={colIndex}
-                            value={value}
-                            onClick={checkTile}
-                        />
-                    ))}
-                </div>
-            ))}
-            
+if ( ((x > 0 && y > 0 && x <= 7 && y <= 7) && matriz[x - 1][y] === oppositeColor )|| matriz[x + 1][y] === oppositeColor || matriz[x][y - 1] === oppositeColor || matriz[x][y + 1] === oppositeColor || matriz[x - 1][y - 1] === oppositeColor || matriz[x - 1][y + 1] === oppositeColor || matriz[x + 1][y - 1] === oppositeColor || matriz[x + 1][y + 1] === oppositeColor) {
+    setMatriz((matriz) => {
+      const newMatriz = [...matriz];
+      newMatriz[x][y] = value;
+      return newMatriz;
+    });
+} else{ return false }
+
+    setTurn(!turn);
+
+  };
+
+  const Tile = ({ row, col, value, onClick }) => (
+    <div
+      onClick={() => onClick(row, col)}
+      className="flex items-center justify-center bg-green-600 h-20 w-20 border-solid border-2 border-green-800"
+    >
+      {value !== 0 && (
+        <div
+          className={`${
+            value === 1 ? "bg-white" : "bg-black"
+          } h-16 w-16 rounded-full`}
+        />
+      )}
+    </div>
+  );
+
+  return (
+    <div className="mt-14">
+      {matriz.map((row, rowIndex) => (
+        <div key={rowIndex} className="flex justify-start ml-44">
+          {row.map((value, colIndex) => (
+            <Tile
+              key={colIndex}
+              row={rowIndex}
+              col={colIndex}
+              value={value}
+              onClick={checkTile}
+            />
+          ))}
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Board;
