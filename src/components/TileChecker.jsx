66b871860e7b845c4b043739209520f1a6a0c5db
Angela <angela@Angelas-MacBook-Pro-4.local>
2024-09-15 22:25:32 +0100

    change in logic, the turn doesn't change if htere is not a possible move

--- src/components/TileChecker.jsx
+++ src/components/TileChecker.jsx
@@ -1,52 +1,49 @@
 import Swal from 'sweetalert2';
 import { Tile } from './Tile';
 
-
 const TileChecker = ({ x, y, endGame, matriz, setMatriz,
     setTurn, turn, allowDiagonal, black, white, boardSize, setEndGame, empty }) => {
 
     const checkTile = () => {
-
- /*  If the game is not over, begin with the verifications */
+        /* If the game is not over, begin with the verifications */
         if (!endGame) { }
         const value = turn ? black : white;
         const oppositeColor = turn ? white : black;
-/* Create a copy of the matrix to update it during the game */
+        /* Create a copy of the matrix to update it during the game */
         const newMatriz = [...matriz];
-/* tIf it's black's turn, change the turn to white's */
-        if (turn === false) {
-            setTurn(true);
-            setEndGame(false);
-        }
+
+        /* If the tile is empty, check if a move can be made */
         if (newMatriz[x][y] === empty) {
-/* Check if the tile where the token is to be placed is empty*/
             let shouldFlip = false;
 
-/* Check all adjacent positions (-1, 0, 1) in rows and columns */
+            /* Check all adjacent positions (-1, 0, 1) in rows and columns */
             for (let i = -1; i <= 1; i++) {
                 for (let j = -1; j <= 1; j++) {
-/* If i and j are 0, continue with the rest of the verifications without checking that tile */
+                    /* If i and j are 0, continue with the rest of the verifications without checking that tile */
                     if (i === 0 && j === 0) {
                         continue;
                     }
-                    
-/* Check if diagonal, horizontal or vertical moves are allowed  */
+
+                    /* Check if diagonal, horizontal or vertical moves are allowed  */
                     if (allowDiagonal || i === 0 || j === 0) {
-/*  If the neighboring tile contains a token of the opposite color*/
+                        /* If the neighboring tile contains a token of the opposite color */
                         if (matriz[x + i]?.[y + j] === oppositeColor) {
                             let row = x + i;
                             let col = y + j;
-/* Continue checking in that direction while there are tokens of the opposite color*/
+                            /* Continue checking in that direction while there are tokens of the opposite color */
                             while (matriz[row]?.[col] === oppositeColor) {
                                 row += i;
                                 col += j;
                             }
-/* If it finds a token of the same color at the end of the direction, it can flip the tokens in that directiond*/
+                            /* If it finds a token of the same color at the end of the direction, it can flip the tokens in that direction */
                             if (matriz[row]?.[col] === value) {
-                                for (let k = i, l = j; matriz[x + k]?.[y + l] === oppositeColor;
-                                    k += i, l += j) {
+                                for (
+                                    let k = i, l = j;
+                                    matriz[x + k]?.[y + l] === oppositeColor;
+                                    k += i, l += j
+                                ) {
                                     newMatriz[x + k][y + l] = value;
-/* Mark that the tokens should be flipped  */
+                                    /* Mark that the tokens should be flipped  */
                                 }
                                 shouldFlip = true;
                             }
@@ -55,29 +52,27 @@ const TileChecker = ({ x, y, endGame, matriz, setMatriz,
                 }
             }
 
-/* If tokens should be flipped, update the matrix and change the turn */
+            /* If tokens should be flipped, update the matrix and change the turn */
             if (shouldFlip) {
                 newMatriz[x][y] = value;
                 setMatriz(newMatriz);
-                setTurn(!turn);
-                return true;
-/* Indicate that the move was valid */
+                setTurn(!turn); // Change turn only if the move was valid
+                return true; /* Indicate that the move was valid */
 
-/* If tokens cannot be flipped in any direction, show an alert and return false */
+            /* If tokens cannot be flipped in any direction, show an alert and return false */
             } else {
                 Swal.fire({
                     icon: 'warning',
                     title: 'Are you sure?',
                     text: 'This move is not allowed!',
                 });
-                return false;
-/* Indicate that the move was not allowed */
+                return false; /* Indicate that the move was not allowed */
             }
         }
-        return false;
-/* If the tile is not empty, return false to disallow the player from placing a token on 
-the board in that position */
+        return false; /* If the tile is not empty, return false to disallow the player from placing a token on 
+        the board in that position */
     };
+
     return (
         <Tile value={matriz[x][y]}
             onClick={checkTile}
@@ -86,4 +81,4 @@ the board in that position */
     );
 };
 
-export { TileChecker }
+export { TileChecker };
